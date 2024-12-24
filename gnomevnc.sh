7218b9f6ec9ddd9fc2d4e18e7a56f3b6d164fbb5
William <126926699+GitXpresso@users.noreply.github.com>
2024-12-23 23:26:38 -0500

    Update gnomevnc.sh

--- gnomevnc.sh
+++ gnomevnc.sh
@@ -1,2 +1,4 @@
 sudo apt update && sudo apt upgrade
+echo -e "Installing Required Packages (tigervnc-standalone-server, tigervnc-common, tigervnc-xorg-extension, tigervnc-viewer)"
 sudo apt install -y tigervnc-standalone-server tigervnc-common tigervnc-xorg-extension tigervnc-viewer
+
