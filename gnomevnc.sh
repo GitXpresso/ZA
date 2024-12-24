665b9ec7a646c4c22eb9c32867f34034de5f862c
William <126926699+GitXpresso@users.noreply.github.com>
2024-12-24 00:00:51 -0500

    Update gnomevnc.sh

--- gnomevnc.sh
+++ gnomevnc.sh
@@ -3,9 +3,9 @@ echo -e "Installing gnome vnc in"
 echo -e "3"
 echo -e "2" 
 echo -e "1"
-sudo apt update && sudo apt upgrade
+sudo apt -y update && sudo apt -y upgrade
 echo -e "Installing Required Packages (tigervnc-standalone-server, tigervnc-common, tigervnc-xorg-extension, tigervnc-viewer)"
 sudo apt install -y tigervnc-standalone-server tigervnc-common tigervnc-xorg-extension tigervnc-viewer
 echo -e "Installing (gnome-session and gnome-terminal)"
 sudo apt install -y gnome-session gnome-terminal
-tigervncserver -xstartup /usr/bin/gnome-session -SecurityTypes VncAuth,TLSVnc -geometry 800x600 -localhost no :2
+tigervncserver -xstartup /usr/bin/gnome-session -SecurityTypes VncAuth,TLSVnc -geometry 800x600 -localhost no :1
