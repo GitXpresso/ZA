413dd7836446c4922c85e387c3e7ea7e2d1fac92
William <126926699+GitXpresso@users.noreply.github.com>
2024-12-23 23:31:50 -0500

    Update gnomevnc.sh

--- gnomevnc.sh
+++ gnomevnc.sh
@@ -1,3 +1,4 @@
+echo "Script isnt fully automatic just need to setup the keyboard layout in gnome installation"
 echo -e "Installing gnome vnc in"
 echo -e "3"
 echo -e "2" 
@@ -5,4 +6,5 @@ echo -e "1"
 sudo apt update && sudo apt upgrade
 echo -e "Installing Required Packages (tigervnc-standalone-server, tigervnc-common, tigervnc-xorg-extension, tigervnc-viewer)"
 sudo apt install -y tigervnc-standalone-server tigervnc-common tigervnc-xorg-extension tigervnc-viewer
-
+echo -e "Installing (gnome-session and gnome-terminal)"
+sudo apt install -u gnome-session gnome-terminal
