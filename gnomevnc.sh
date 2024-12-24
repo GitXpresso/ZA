d1bea2e906bab3aae4242ed0aecf400f1afbf7e3
William <126926699+GitXpresso@users.noreply.github.com>
2024-12-23 23:28:06 -0500

    Update gnomevnc.sh
    Added countdown

--- gnomevnc.sh
+++ gnomevnc.sh
@@ -1,3 +1,7 @@
+echo -e "Installing gnome vnc in"
+echo -e "3"
+echo -e "2" 
+echo -e "1"
 sudo apt update && sudo apt upgrade
 echo -e "Installing Required Packages (tigervnc-standalone-server, tigervnc-common, tigervnc-xorg-extension, tigervnc-viewer)"
 sudo apt install -y tigervnc-standalone-server tigervnc-common tigervnc-xorg-extension tigervnc-viewer
